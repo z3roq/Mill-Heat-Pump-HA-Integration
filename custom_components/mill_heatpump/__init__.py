33335c13b2d52955e7e5c23c4a434875fcda5216
Janne <78497789+z3roq@users.noreply.github.com>
2025-08-25 20:01:23 +0300

    Create __init__.py

---
+++ custom_components/mill_heatpump/__init__.py
@@ -0,0 +1,11 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return await hass.config_entries.async_unload_platforms(entry, ["climate"])
